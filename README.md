ed936a80289e33ecdd484853265460e95087733e
Mika Nokka <mika.nokka1@gmail.com>
2023-01-07 14:43:39 +0200

    Cleaned. Readme added

---
+++ README.md
@@ -0,0 +1,27 @@
+#  Simple Ceasar crypter-decrypter 
+  
+Uses defined letters array (letters, numbers), not defined letters will be set as ? when crypting the message   
+Letter shifting value: negative=backwards in letterlist, positive forwards. 0 does not crypt at all
+
+Execute: python3 crypter.py    
+
+Use menus for operations:    
+
+
+Ceasar Crypter, Current shifter value: -4    
+Used alphabet: [' ', '.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '?', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Å', 'Ä', 'Ö']    
+Number of letters: 71    
+
+*********** Select operation *************  
+1 - Crypt  
+2 - Decrypt  
+3 - Crypting shifter value (current: -4 )  
+x - Quit  
+?1  
+String to be crypted:Kissa  
+Crypted: Geoo9  
+
+
+
+
+
--- crypter.py
+++ crypter.py
@@ -1,96 +1,108 @@
 #
-# Simple Ceasar crypter-decrypter
-# Uses hardcoded shifting value (negative=backwards in letterlist,positive forward)
-
-
-#letter to be shifted value
-shifter=-4
-
-#alphabets to be used
-letters=[" ","1","2","3","4","5","6","7","8","9","0","-","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö"]
+# Simple Ceasar crypter-decrypter ,  mika.nokka1@gmail.com Jan 2023
+# Uses defined letters array. Not defined letters in tobe-crypted message will be set as ?
+# Shifting value: negative=backwards in letterlist,positive forward. 0 does not crypt at all
+#
 
 
 
-# Defining main function
+#letters to be used
+letters=[" ",".","1","2","3","4","5","6","7","8","9","0","-","?",
+         "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","å","ä","ö",
+         "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Å","Ä","Ö"]
+
+
+
+
 def main():
-        print("Ceasar Crypter, fixed shifting value:",shifter)
-        print("Used alphabet:",letters)
-        print ("Number of letters:",len(letters))
 
-        menu()
+        #initial crypting shifter value 
+        shifter=-4 
+        
+        print("Ceasar Crypter, Current shifter value:",shifter)
+        print("Used alphabet:",letters)
+        print("Number of letters:",len(letters))
+
+        menu(shifter)
         
         while True:
-            x=input()
+            x=input("?")
         
             if (x=="1"):
-                #print("Crypting")
-                crypt()
-                menu()
+                crypt(shifter)
+                menu(shifter)
             elif (x=="2"):
-                #print("Decrypting")
-                decrypt()
-                menu()
+                decrypt(shifter)
+                menu(shifter)
+            elif (x=="3"):
+                shifter=changeshifter(shifter)
+                menu(shifter)                    
             elif (x=="x"):
-                #print ("Ending") 
                 break   
             else:
                 print ("Wrong selection")
-                menu()
+                menu(shifter)
  
  
-def menu():
-        print("*** Select operation ***")
-        print("1-crypt")
-        print("2-decrypt")
-        print ("x - end") 
+def menu(shifter):
+        print("")
+        print("*********** Select operation *************")
+        print("1 - Crypt")
+        print("2 - Decrypt")
+        print("3 - Crypting shifter value (current:",shifter,")")
+        print("x - Quit") 
         
-def crypt():
-    word=input("To be crypted:")
+        
+def crypt(shifter):
+    word=input("String to be crypted:")
     wlen=len(word)
     cryptedword=""
     for i in range(wlen):
-        cletter=cryptLetter(word[i])
+        cletter=cryptLetter(word[i],shifter)
         cryptedword=cryptedword+cletter
-    
-    print("Crypted word:",cryptedword)
+    print("Crypted:",cryptedword)
 
     
-def cryptLetter(letter):
+def cryptLetter(letter,shifter):
+    
+    #not defined letters , force set as ?
+    if letter not in letters:
+        letter="?"
+    
     location=(letters.index(letter))
     alphaslen=len(letters)
     z=location+shifter
     
-    # over maximum len
+    
+    #over maximum len
     if (z>=alphaslen):
         positiveover=z-alphaslen
         cryptedletter=letters[positiveover]
     #under location 0
-    elif (z<=0):
+    elif (z<0):
         negativeover=alphaslen+z
         cryptedletter=letters[negativeover]
     #inside letters range
     else:
         cryptedletter=letters[z]
-
     return cryptedletter    
 
-def decrypt():  
-    word=input("To be decrypted:")
+def decrypt(shifter):  
+    word=input("String to be decrypted:")
     wlen=len(word)
     decryptedword=""
     for i in range(wlen):
-        dletter=decryptLetter(word[i])
-        decryptedword=decryptedword+dletter
-    
-    print("Decrypted word:",decryptedword)
+        dletter=decryptLetter(word[i],shifter)
+        decryptedword=decryptedword+dletter    
+    print("Decrypted:",decryptedword)
 
     
-def decryptLetter(letter):              
+def decryptLetter(letter,shifter):              
     location=(letters.index(letter))     
     alphaslen=len(letters)
     z=location-shifter
     
-    # over maximum len
+    #over maximum len
     if (z>=alphaslen):
         positiveover=z-alphaslen
         decryptedletter=letters[positiveover]
@@ -98,13 +110,19 @@ def decryptLetter(letter):
     elif (z<=0):
         negativeover=alphaslen+z
         decryptedletter=letters[negativeover]
-        #inside letters range
+    #inside letters range
     else:
         decryptedletter=letters[z]
-
     return decryptedletter    
 
-
+def changeshifter(shifter):
+    print ("*************************************************************")
+    print("Used alphabets:",letters)
+    print ("Number of letters:",len(letters))
+    print("Current crypter letter shifter value:",shifter)
+    print ("(Value 0; no crypting done)")
+    shifter=input("New shifter value?")
+    return int(shifter)
 
 if __name__=="__main__":
     main()
